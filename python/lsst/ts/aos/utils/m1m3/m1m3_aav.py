4255a6efd2864faabf8d317fe3c7d43cdec0fdbb
Petr Kubánek <pkubanek@lsst.org>
2023-07-20 22:14:05 +0200

    WIP - plotting, interpolating, ..

--- python/lsst/ts/aos/utils/m1m3/m1m3_aav.py
+++ python/lsst/ts/aos/utils/m1m3/m1m3_aav.py
@@ -22,15 +22,18 @@
 import argparse
 import asyncio
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from astropy.time import Time
+from astropy.time import Time, TimeDelta
 from lsst_efd_client import EfdClient
 
 
 class AccelerationAndVelocities:
     def __init__(self, efd_name: str):
         self.efd_name = efd_name
+        self.actual_velocity_limit = 0.03
+        self.demand_velocity_limit = 1e-5
 
     async def find_non_zero(
         self, start_time: Time, end_time: Time
@@ -42,8 +45,10 @@ class AccelerationAndVelocities:
                 "SELECT actualVelocity, demandVelocity "
                 f'FROM "efd"."autogen"."lsst.sal.MTMount.{axis}" '
                 f"WHERE time > '{start_time.isot}+00:00' AND time < '{end_time.isot}+00:00' "
-                "AND ((abs(actualVelocity) > 0.01) or (abs(demandVelocity) > 0.01))"
+                f"AND ((abs(actualVelocity) > {self.actual_velocity_limit:f}) or "
+                f"(abs(demandVelocity) > {self.demand_velocity_limit:f}))"
             )
+            print("Quering EFD:", query)
             ret = await self.client.influx_client.query(query)
             ret["timediff"] = ret.index.to_series().diff()
 
@@ -70,19 +75,65 @@ class AccelerationAndVelocities:
             (df_moving.timediff > np.timedelta64(1, "s")) | (df_moving.timediff is None)
         ].iterrows():
             end = index - row["timediff"]
-            if start is not None:
+            if start is not None and (end - start) > TimeDelta(0.5, format="sec"):
                 ret.append([start, end, len(df_notmoving[start:end].index) == 0])
             start = index
 
         return pd.DataFrame(ret, columns=["start", "end", "use"])
 
-    async def fit_aav(
-        self, start_time: Time, end_time: Time, extrapolate_baseline: bool
-    ) -> None:
+    async def load_hardpoints(self, start_time: Time, end_time: Time) -> pd.DataFrame:
+        print(f"Retrieving HP data for {start_time} - {end_time}..", end="")
+        ret = await self.client.select_time_series(
+            "lsst.sal.MTM1M3.hardpointActuatorData",
+            [f"measuredForce{hp}" for hp in range(6)]
+            + [f"f{a}" for a in "xyz"]
+            + [f"m{a}" for a in "xyz"],
+            Time(start_time),
+            Time(end_time),
+        )
+        print("OK")
+        return ret
+
+    async def fit_aav(self, start_time: Time, end_time: Time, plot: bool) -> None:
         self.client = EfdClient(self.efd_name)
+        self._plot = plot
+
         elevations, azimuths = await self.find_non_zero(start_time, end_time)
         intervals = await self.find_applicable_times(elevations, azimuths)
-        print(intervals)
+        fit = pd.DataFrame()
+        for index, row in intervals[intervals.use].iterrows():
+            block_start = row["start"] - pd.Timedelta("2s")
+            block_end = row["end"] + 2
+            hardpoints = await self.load_hardpoints(block_start, block_end)
+            elevations_hardpoints = elevations[
+                (elevations.index >= block_start) & (elevations.index <= block_end)
+            ]
+            fit = pd.concat(
+                [
+                    fit,
+                    pd.merge(
+                        elevations_hardpoints,
+                        hardpoints,
+                        how="outer",
+                        left_index=True,
+                        right_index=True,
+                    ),
+                ]
+            )
+        print("Hardpoint data retrieved")
+        print(elevations_hardpoints.describe())
+        print(fit)
+        fit.drop(columns=["timediff"], inplace=True)
+        print(fit.dtypes)
+        # fit = fit.interpolate(method="polynomial", order=5)
+        fit = fit.interpolate(method="time")  # , order=5)
+        print(fit.describe())
+        print(fit)
+        fit.to_hdf("fit.hd5", "fit")
+        plt.plot(fit["demandVelocity"], fit["my"], ".")
+        plt.plot(fit["demandAcceleration"], fit["my"], ".")
+        plt.show()
+        # print(df)
 
 
 def parse_arguments() -> argparse.Namespace:
@@ -113,16 +164,22 @@ def parse_arguments() -> argparse.Namespace:
     parser.add_argument(
         "--m1m3-config", default=None, help="Path to the LUT file to be updated"
     )
+    parser.add_argument(
+        "--plot", default=False, action="store_true", help="Plot graphs during"
+    )
 
     return parser.parse_args()
 
 
-def run() -> None:
+async def run_loop() -> None:
     args = parse_arguments()
 
     aav = AccelerationAndVelocities(args.efd)
+    await aav.fit_aav(args.start_time, args.end_time, args.plot)
 
-    asyncio.run(aav.fit_aav(args.start_time, args.end_time, False))
+
+def run() -> None:
+    asyncio.run(run_loop())
 
 
 if __name__ == "__main__":
